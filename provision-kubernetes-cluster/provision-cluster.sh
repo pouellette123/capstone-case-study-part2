117deb9486640e8513ac77a94bfbce7574fcc34c
Paul Ouellette <pouellette123es@gmail.com>
2021-03-01 14:07:05 -0500

    updated provision script, added config-option.txt file

--- provision-kubernetes-cluster/provision-cluster.sh
+++ provision-kubernetes-cluster/provision-cluster.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo  "Enter Cluster Provision Type."
-echo -n "1 for kind, 2 for AWS EKS: "
+echo    "Enter Cluster Provision Type."
+echo -n "   1 for kind, 2 for AWS EKS: "
 read num
 
 if [[ $num -lt 1 || $num -gt 2 ]]; then
 echo "Invalid entry, try again."
+exit
 elif [ $num -eq 1 ]; then
    kind create cluster --name capstone-part2  --config ./kind/kind-config.yml
    CLUSTER_CONTEXT="kind-capstone-part2"
@@ -16,4 +17,5 @@ elif [ $num -eq 2 ]; then
    CLUSTER_CONTEXT=`kubectl config get-contexts | grep $CLUSTER_RANDOM | cut -f13 -d ' '`
    cd ..
 fi
+echo $num > ./config-option.txt
 kubectl config use-context $CLUSTER_CONTEXT
